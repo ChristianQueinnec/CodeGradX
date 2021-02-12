7a3dbc490ff53934672c47ce527b31e8359f5025
Christian Queinnec <Christian.Queinnec@gmail.com>
2021-02-12 10:27:59 +0100

    adapt xml-js to xml2js interface

--- src/parsexml.mjs
+++ src/parsexml.mjs
@@ -1,5 +1,5 @@
 // parsexml.js
-// Time-stamp: "2021-02-11 17:54:47 queinnec"
+// Time-stamp: "2021-02-12 10:26:12 queinnec"
 
 import { CodeGradX } from 'codegradx';
 //import xml2js from 'codegradx/src/xml2js';
@@ -17,16 +17,52 @@ import { convert } from 'codegradx/src/xml2js';
 
 export function parsexml (xml) {
     const options = {
+        compact: true,
+        trim: true,
         ignoreComment: true,
         ignoreInstruction: true,
         ignoreDoctype: true,
         ignoreDeclaration: true
     };
     try {
-        const result = convert.xml2js(xml, options);
+        let result = convert.xml2js(xml, options);
+        result = convert_(result);
         return Promise.resolve(result);
     } catch (exc) {
         return Promise.reject(exc);
+    }
+}
+
+/**
+   xml-js names $ as _attributes and _ as _text so convert these names
+   back to what xml2js used.
+ */
+
+function convert_ (o) {
+    if ( typeof o === 'object' ) {
+        const keys = Object.keys(o);
+        for ( let key of keys ) {
+            if ( key === '_text' ) {
+                o._ = o._text;
+                delete o._text;
+                key = '_';
+            } else if ( key === '_attributes' ) {
+                o.$ = o._attributes;
+                delete o._attributes;
+                key = '$';
+            }
+            o[key] = convert_(o[key]);
+        }
+        if ( keys.length === 1 &&
+             keys[0] === '_text' ) {
+            return o._;
+        } else {
+            return o;
+        }
+    } else if ( Array.isArray(o) ) {
+        return o.map(convert_);
+    } else {
+        return o;
     }
 }
 
